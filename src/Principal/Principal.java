63c05d14f46b9c0acbd8803c7798db5a84cfbc4e
gustavoabrahao <eugustavinseguindo@gmail.com>
2025-04-08 21:08:38 -0300

    add

---
+++ src/Principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Rogergaydaocu");
+    }
+    
+}
