19a3385a4aaeafdaff52fea1950882e12e0b5334
gustavoabrahao <eugustavinseguindo@gmail.com>
2025-04-30 07:46:44 -0300

    Ajustes opções

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -6,6 +6,6 @@ public class Principal {
 
    
     public static void main(String[] args) {
-        JOptionPane.showInputDialog("1 - Cadastrar Produtos \n2 - Lista de Preços \n3 - Balanço Financeiro \n4 - Relatório Produtos \n5 - Relatório Quantidade \n6 - Sair");
+        JOptionPane.showInputDialog("1 - Cadastrar Produtos \n2 - Lista de Preços \n3 - Balanço Financeiro \n4 - Relatório Quantidade Mínima \n5 - Relatório Quantidade Máxima \n6 - Sair");
     }
 }
