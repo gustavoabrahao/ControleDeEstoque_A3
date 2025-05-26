35c5668efb55619c5b4d8b4c57a5d3044c100ff8
carlosEduWeiss <chiclamaw@gmail.com>
2025-05-26 19:17:46 -0300

    Ajustes opções

---
+++ src/dao/ProdutoDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author chicl
+ */
+public class ProdutoDAO {
+    public void criarProduto(){
+        
+    }
+    public void LerProduto(){
+        
+    }
+    public void AtualizarProduto(){
+        
+    }
+    public void DeletarProduto(){
+        
+    }
+}
---
+++ src/modelo/produto.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author chicl
+ */
+public class produto {
+    private int id;
+    private String nome;
+    private int unidade;
+    private double preço;
+    private int min;
+    private int max;
+    private String categoria;
+
+    public produto() {
+    }
+
+    public produto(int id, String nome, int unidade, double preço, int min, int max, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.unidade = unidade;
+        this.preço = preço;
+        this.min = min;
+        this.max = max;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(int unidade) {
+        this.unidade = unidade;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(double preço) {
+        this.preço = preço;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public String VerificacaoDeQuantidade(){
+        if (this.getUnidade()<this.getMin()){
+            return "A quantidade do produto está muito baixa.";
+            
+        }else if(this.getUnidade()>this.getMax()){
+            return "A quantidade do produto é muito alta.";
+        }else{
+            return "produto registrado com sucesso.";
+        }
+    }
+}
