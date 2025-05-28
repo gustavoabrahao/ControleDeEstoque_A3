999a410e0fc6b26dc86f8b828f8ccb2b56c0bd42
bnsant <bernardosanlima@gmail.com>
2025-05-28 19:12:27 -0300

    mudança do nome do jframe para melhor gestão

--- src/visao/FrmListadeProdutos.java
+++ src/visao/FrmListadeProdutos.java
@@ -8,12 +8,12 @@ package visao;
  *
  * @author berna
  */
-public class FrmCadastro extends javax.swing.JFrame {
+public class FrmListadeProdutos extends javax.swing.JFrame {
 
     /**
      * Creates new form FrmCadastro
      */
-    public FrmCadastro() {
+    public FrmListadeProdutos() {
         initComponents();
     }
 
@@ -179,20 +179,21 @@ public class FrmCadastro extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmListadeProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmListadeProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmListadeProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmListadeProdutos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmCadastro().setVisible(true);
+                new FrmListadeProdutos().setVisible(true);
             }
         });
     }
