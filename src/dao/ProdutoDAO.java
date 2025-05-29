73bdbfbe3ad44ffbb4642468362559b949a6ee3e
Gabriel <bibizingmtts@gmail.com>
2025-05-29 19:39:53 -0300

    chore: clean pass on ProdutoDAO logic flow

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -1,5 +1,5 @@
-
 package dao;
+
 import java.lang.RuntimeException;
 import java.text.ParseException;
 import java.sql.Connection;
@@ -9,79 +9,73 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JOptionPane; 
+import javax.swing.JOptionPane;
+
 import modelo.Produto;
 import visao.FrmListadeProdutos;
 import visao.FrmTelaPrincipal;
-        
 
 public class ProdutoDAO {
+
     Produto produto = new Produto();
-    
-    public void CadastrarProduto(){
+
+    public void CadastrarProduto() {
         Connection conn = null;
         PreparedStatement st = null;
-        
-        try{
+
+        try {
             conn = Conexao.conectar();
-            
-            
-        st = conn.prepareStatement("INSERT INTO produto "
-                + "(nome, unidade, preco, min, max, categoria) "
-                + "VALUES "
-                + "(?,?,?,?,?,?) ");
-        
-        st.setString(1,produto.getNome());
-        st.setInt(2, produto.getUnidade());
-        st.setDouble(3, produto.getPreco());
-        st.setInt(4, produto.getMin());
-        st.setInt(5, produto.getMax());
-        st.setString(6, produto.getCategoria());
-        
-        st.executeUpdate();
-  
-        System.out.println("Produto cadastrado com sucesso!");
-                
-        
-        }catch (SQLException e){
-            e.printStackTrace();    
-        }finally{
-            try{
+
+            st = conn.prepareStatement(
+                    "INSERT INTO produto " +
+                            "(nome, unidade, preco, min, max, categoria) " +
+                            "VALUES (?, ?, ?, ?, ?, ?)"
+            );
+
+            st.setString(1, produto.getNome());
+            st.setInt(2, produto.getUnidade());
+            st.setDouble(3, produto.getPreco());
+            st.setInt(4, produto.getMin());
+            st.setInt(5, produto.getMax());
+            st.setString(6, produto.getCategoria());
+
+            st.executeUpdate();
+
+            System.out.println("Produto cadastrado com sucesso!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
                 if (st != null) st.close();
                 Conexao.desconectar();
-                
-            }catch(SQLException e){
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
-            
         }
-        
-        
-     
     }
-    
-    public void ProcurarProdutoID(){
-        
+
+    public void ProcurarProdutoID() {
+        // A implementar
     }
-    public void ProcurarProdutoNome(){
-        
+
+    public void ProcurarProdutoNome() {
+        // A implementar
     }
-    
-    public void AtualizarProduto(){
+
+    public void AtualizarProduto() {
         Connection conn = null;
         PreparedStatement st = null;
-//        try{
+
+        // A implementar
+//        try {
 //            conn = Conexao.conectar();
-//            
 //            st = conn.prepareStatement(
-//                    "UPDATE produto ")
-//
-                    
-        
-        
+//                "UPDATE produto SET ..."
+//            );
     }
-    
-    public void DeletarProdutoID(){
-        
+
+    public void DeletarProdutoID() {
+        // A implementar
     }
 }
